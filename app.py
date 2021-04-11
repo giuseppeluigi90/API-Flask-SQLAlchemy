3cce8428ded3022e2900a7d485c274f6c09c66f5
giuseppe90 <giuseppeluigi90@outlook.com>
2021-04-11 18:09:17 -0500

    Estable

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-from src.models import Prueba
 from flask import Flask, render_template, redirect, url_for, request, flash, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from SQLAlchemy import Column, Integer, String
 from flask_marshmallow import Marshmallow
 
-Prueba = Prueba()
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://scx:y2K.scx@scx/scxcp'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db = DB(app)
+db = SQLAlchemy(app)
 ma = Marshmallow(app)
+
+class Prueba(db.Model):
+    id = Column(Integer, primary_key = True)
+    name = Column(String)
+    address = Column(String)
+    email = Column(String)
+
+    def __init__(self, name, address, email):
+        self.name = name
+        self.address = address
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+# db.create_all()
 
 
 class PruebaSchema(ma.Schema):
--- src/models.py
+++ src/models.py
@@ -1,17 +1,2 @@
-from flask_sqlalchemy import SQLAlchemy, Column, Integer, String
 
-class Prueba(db.Model):
-    id = Column(Integer, primary_key = True)
-    name = Column(String)
-    address = Column(String)
-    email = Column(String)
 
-    def __init__(self, name, address, email):
-        self.name = name
-        self.address = address
-        self.email = email
-
-    def __repr__(self):
-        return '<User %r>' % self.name
-
-# db.create_all()
